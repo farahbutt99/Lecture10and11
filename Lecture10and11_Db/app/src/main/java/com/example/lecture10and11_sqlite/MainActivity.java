a90205734e47d0fc5d80935443201f804135e7af
farahbutt99 <mcsf19a021@pucit.edu.pk>
2021-05-25 12:51:17 +0500

    Find each field by id in main activity

--- Lecture10and11_Db/app/src/main/java/com/example/lecture10and11_sqlite/MainActivity.java
+++ Lecture10and11_Db/app/src/main/java/com/example/lecture10and11_sqlite/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.lecture10and11_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonAdd, buttonViewAll;
+    EditText editName, editAge;
+    Switch switchIsActive;
+    ListView listViewCustomer;
+    ArrayAdapter<CustomerModel> arrayAdapter;
+    DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonViewAll = findViewById(R.id.buttonViewAll);
+        editName = findViewById(R.id.editTextName);
+        editAge = findViewById(R.id.editTextAge);
+        switchIsActive = findViewById(R.id.switchCustomer);
+        listViewCustomer = findViewById(R.id.listViewCustomer);
     }
 }
